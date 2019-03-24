ba48dc54e55adce0b38ddc5b85b707858fed3f52
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-24 15:49:57 +0530

    MultiplicationFloat

---
+++ MultiplicationFloat/MultiplicationFloat.java
@@ -0,0 +1,9 @@
+public class MultiplicationFloat {
+    public static void main(String args[]) {
+        float a,b,c;
+        a = 5.7f;
+        b = 7.7f;
+        c = a * b;
+        System.out.println("Product = " + c);
+    }
+}
