1e6131172ebac156e45cb1e08ac280ad99f33a2f
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-24 15:55:30 +0530

    DivisionFloat

---
+++ DivisionFloat/DivisionFloat.java
@@ -0,0 +1,9 @@
+public class DivisionFloat {
+    public static void main(String args[]) {
+        double a,b,c;
+        a = 5;
+        b = 7;
+        c = a / b;
+        System.out.println("Float Division = " + c);
+    }
+}
