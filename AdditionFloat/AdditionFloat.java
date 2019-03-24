7f2b9875f2930a0775e301855f8a112246ae0263
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-24 15:40:37 +0530

    AdditionFloat

---
+++ AdditionFloat/AdditionFloat.java
@@ -0,0 +1,9 @@
+public class AdditionFloat{
+    public static void main(String args[]){
+        double a,b,c;
+        a = 3.3;
+        b = 5.0;
+        c = a + b;
+        System.out.println("Float Addition = " + c);
+    }
+}
--- SubtractionFloat/SubtractionFloat.java
+++ SubtractionFloat/SubtractionFloat.java
@@ -1,8 +1,8 @@
-public class SubtractionFloat{
-  public static void main(String args[]){
-    double a,b,c;
-    a = 1.4;
-    b = 3.5;
+public class SubtractionFloat {
+  public static void main(String args[]) {
+    float a,b,c;
+    a = (float)1.4;
+    b = (float)3.5;
     c = a-b;
     System.out.println("Float Difference = " + c);
   }
