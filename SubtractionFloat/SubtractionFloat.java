6fe277b4a3e311c1c099a936cfa1f88c45e138e8
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-24 10:54:47 +0530

    SubtractionFloat

---
+++ SubtractionFloat/SubtractionFloat.java
@@ -0,0 +1,9 @@
+public class SubtractionFloat{
+  public static void main(String args[]){
+    double a,b,c;
+    a = 1.4;
+    b = 3.5;
+    c = a-b;
+    System.out.println("Float Difference = " + c);
+  }
+}
