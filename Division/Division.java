bde0b755958a04196458e681dc871f7ecf5e3c20
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-24 15:56:14 +0530

    Division

---
+++ Division/Division.java
@@ -0,0 +1,9 @@
+public class Division {
+    public static void main(String args[]) {
+        int a,b,c;
+        a = 4;
+        b = 2;
+        c = a / b;
+        System.out.println("Division = " + c);
+    }
+}
