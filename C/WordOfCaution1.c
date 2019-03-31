c08b936cefc2c571bdd7715d5ab4e23108e47c35
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-31 17:08:36 +0530

    Word Of caution new program

---
+++ C/WordOfCaution1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("Enter the value of i \n");
+    scanf("%d",&i);
+    if(i==5){
+        printf("You have entered 5\n");
+    } else {
+        printf("You have entered something else other than 5\n");
+    }
+}
