b22926d66f275ff4386164a54979e15e4870f207
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-03-31 11:11:36 +0530

    Hello World without semi-colon

--- C/real_thing.c
+++ C/real_thing.c
@@ -1,13 +1,7 @@
+/* PRINT HELLO WORLD WITHOUT USING SEMI-COLON*/
 #include<stdio.h>
 int main(){
-    int a;
-    if(3+2%5){
-        printf("This works\n");
-    }
-    if(a=10){
-        printf("This also works\n");
-    }
-    if(-4){
-        printf("Surprisingly even this works\n");
+    if(printf("Hello World\n")){
+        printf("Hi\n");
     }
 }
