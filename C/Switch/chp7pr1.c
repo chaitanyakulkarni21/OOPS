1f9a471034592b3703f49d90642a1fb43771c820
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-05 21:34:52 +0530

    Switch

---
+++ C/Switch/chp7pr1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i=2;
+    switch(i){
+        case 1 : printf("Iam in case 1\n");
+        case 2 : printf("Iam in case 2\n");
+        case 3 : printf("Iam in case 3\n");
+        case 4 : printf("Iam in case 4\n");
+    }
+}
