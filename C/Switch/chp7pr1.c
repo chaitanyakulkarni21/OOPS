5d0c901ee6ccf812c534f8c1d4e647df0175e401
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-05 21:39:00 +0530

    Break implemented

--- C/Switch/chp7pr1.c
+++ C/Switch/chp7pr1.c
@@ -3,8 +3,11 @@ int main(){
     int i=2;
     switch(i){
         case 1 : printf("Iam in case 1\n");
+                 break;
         case 2 : printf("Iam in case 2\n");
+                 break;
         case 3 : printf("Iam in case 3\n");
+                 break;
         case 4 : printf("Iam in case 4\n");
     }
 }
