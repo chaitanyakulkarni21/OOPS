0827f594e7fd3cedc089dc96c72965422c259444
Rakshanda Shende <rakshanda.shende@appxbuild.com>
2019-04-05 08:25:51 +0530

    Function pr2

---
+++ C/Functions/pr2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void Italy();
+void Brazil();
+void Argentina();
+int main()
+{
+    Italy();
+    Brazil();
+    Argentina();
+}
+
+void Italy(){
+    printf("Iam in Italy\n");
+}
+void Brazil(){
+    printf("Iam in Brazil\n");
+}
+void Argentina()
+{
+    printf("Iam in Argentina\n");
+}
