f0441e3ea142e1421f75fd5c1c4a4742f9e85e5f
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-06 10:49:12 +0530

    ch8ex1

---
+++ C/Functions/ch8ex1.c
@@ -0,0 +1,14 @@
+/*ch 8 ex 1*/
+#include<stdio.h>
+void display();
+int main()
+{
+	printf("Learn C\n");
+	display();
+	return 0;
+}
+void display()
+{
+	printf("Followed by C++, C#, Java");
+	main();
+}
