6bdd71a9d9f85bf7f0d730fe25a50ee794b3437d
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-08 08:23:05 +0530

    Error solved

--- C/Functions/ch8exB_f.c
+++ C/Functions/ch8exB_f.c
@@ -2,7 +2,7 @@
 void message();
 int main()
 {
-	message(message());// function cannot be called inside a function as a parameter
+	message();// function cannot be called inside a function as a parameter
 	return 0;
 }
 void message()
