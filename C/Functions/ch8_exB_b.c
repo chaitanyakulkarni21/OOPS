875b671814fd9eafda4d17836422ec0aab378571
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-08 08:13:59 +0530

    Error spotted in the program commented

---
+++ C/Functions/ch8_exB_b.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void message();
+int main()
+{
+	int a;
+	a=message();//function return type is void so cannot be assigned to a variable
+}
+void message()
+{
+	printf("Followed by C++,C# and JAVA\n");
+	return;
+}
