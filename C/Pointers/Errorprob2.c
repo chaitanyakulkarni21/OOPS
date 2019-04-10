d439ceca8c2a865f2f8544ee43566ec958c0122f
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-10 08:41:27 +0530

    ErrorProb2, Errors commented

---
+++ C/Pointers/Errorprob2.c
@@ -0,0 +1,15 @@
+/* Error prob 2 */
+#include<stdio.h>
+void jiaayjo (int ,int)	// Error : ; needed
+int main()
+{
+	int p = 23 ,f = 24;
+	jiaayjo(&p,&f);
+	printf("%d %d\n",p,f);
+	return 0;
+}
+void jiaayjo(int q ,int g)
+{
+	q = q + q;
+	g = g + g;
+ } 
