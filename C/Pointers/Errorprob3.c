461dadaebfaf40c9df33325412a9c4ffe25ba0df
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-10 08:50:23 +0530

    ErrorProb3, Errors commented

---
+++ C/Pointers/Errorprob3.c
@@ -0,0 +1,18 @@
+/* Errors Prob3 */
+#include<stdio.h>
+void check (int);
+int main()
+{
+	int k=35,z;
+	z = check(k); // trying to accept a returned value while using void as return type of the function 
+	printf("%d\n",z);
+	return 0;
+}
+void check(m)	// int m should be a paramater instead of m
+{
+	int m;		// re-declaration of m
+	if(m>40)
+		return(1);	// trying to return a value while using void
+	else 
+		return(0);	// trying to return a value while using void
+}
