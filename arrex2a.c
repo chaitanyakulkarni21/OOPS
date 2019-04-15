2328b1c439f47db48a87ea6221ecea938958f1e2
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-15 12:44:00 +0530

    error found

---
+++ arrex2a.c
@@ -0,0 +1,12 @@
+/* Array exercise 2a */
+#include<stdio.h>
+int main()
+{
+	int array[6] = {1,2,3,4,5,6};
+	int i;
+	for(i=0;i<=25;i++)	// No error, but the fo
+	{
+		printf("%d ",array[i]);
+	}
+	return 0;
+}
