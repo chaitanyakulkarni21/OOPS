4d8e2bc3374be4c899d331244f4b6bfc47b33182
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-16 08:48:11 +0530

    2d Array 3

---
+++ C/Arrays/2dArrayex3.c
@@ -0,0 +1,21 @@
+/* Pointer notation to access the array elements in a 2d array */
+#include<stdio.h>
+int main()
+{
+	int s[4][2] = {
+						{1234,56},
+						{1212,33},
+						{1434,80},
+						{1312,78}
+				  };	
+	int i,j;
+	for(i=0;i<=3;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("%d",*(*(s+i)+j));
+		}
+		printf("\n");
+	}
+	return 0;
+}
