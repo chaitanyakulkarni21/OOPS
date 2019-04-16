cfa4c1713e4bf82be496d239359fb6562ab930a5
Chaitanya Kulkarni <chaitanya.kulkarni@synapse.com>
2019-04-16 11:44:13 +0530

    #define used

--- C/Arrays/prod2_1d_arrays.c
+++ C/Arrays/prod2_1d_arrays.c
@@ -1,29 +1,30 @@
 /* Product of two 1 dimensional arrays */
 #include<stdio.h>
+#define MAX 5
 int main()
 {
-	int a[5],b[5],i,p=0;
+	int a[MAX],b[MAX],i,p=0;
 	printf("Enter the array 1 :\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		scanf("%d",&a[i]);
 	}
 	printf("\nEnter the array 2 :\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		scanf("%d",&b[i]);
 	}
 	printf("\nArray 1:\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		printf("%d ",a[i]);
 	}
 	printf("\nArray 2 :\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		printf("%d ",b[i]);
 	}
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		p = p + a[i] * b[i];
 	}
--- C/Arrays/sum2_1d_array.c
+++ C/Arrays/sum2_1d_array.c
@@ -1,34 +1,35 @@
 /* Sum of 2 one dimensional arrays */
 #include<stdio.h>
+#define MAX 5
 int main()
 {
-	int a[5],b[5],c[5],i;
+	int a[MAX],b[MAX],c[MAX],i;
 	printf("Enter the array 1 :\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		scanf("%d",&a[i]);
 	}
 	printf("\nEnter the array 2 :\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		scanf("%d",&b[i]);
 	}
 	printf("\nArray 1:\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		printf("%d ",a[i]);
 	}
 	printf("\nArray 2 :\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		printf("%d ",b[i]);
 	}
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		c[i] = a[i] + b[i];
 	}
 	printf("\nArray 1 + Array 2:\n");
-	for(i=0;i<=4;i++)
+	for(i=0;i<=MAX-1;i++)
 	{
 		printf("%d ",c[i]);
 	}
