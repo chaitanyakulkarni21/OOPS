15edb725f9fecccddb9185ec361b41059eb3ce26
Chaitanya Kulkarni <chaitanya.kulkarni@synapse.com>
2019-04-17 12:21:38 +0530

    String length using string function

---
+++ C/Strings/strlenfun.c
@@ -0,0 +1,12 @@
+/* String length using string function */
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[20];
+	int i,l;
+	printf("Enter a string:\n");
+	scanf("%s",str);
+	l= strlen(str);
+	printf("The length of the string is = %d",l);
+}
