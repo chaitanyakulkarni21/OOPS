558cf1b43ec47d39a29d403471ab0ec4d07e2939
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-18 08:17:26 +0530

    gets used

--- C/Strings/strlen.c
+++ C/Strings/strlen.c
@@ -2,12 +2,15 @@
 #include<stdio.h>
 int main()
 {
-	char str[10],i,l=0;
+	char str[20],i,l=0;
 	printf("Enter a String:\n");
-	scanf("%s",str);
+//	scanf("%s",str);
+	gets(str);	// used gets... gets counts <space>
+	
 	for(i=0;str[i]!='\0';i++)
 	{
 		l++;
 	}
 	printf("Length of the string is : %d\n",l);
+	return 0;
 }
