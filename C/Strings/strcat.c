0e2f8fa09f48fb26d70c0016fbf49a8007c2861a
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-19 08:28:54 +0530

    String Concatenation

--- C/Strings/strcat.c
+++ C/Strings/strcat.c
@@ -10,36 +10,22 @@ int main()
 	scanf("%s",str2);
 	strcat(str1,str2);
 	printf("After concatenation: %s",str1);
-	
-
-	
+	return 0;
 }
 void strcat(char *p,char *q)
 {
 	int i,j;
-	for(i=0;i<10;i++)
-	{
-		printf("char is =%d\n",p[i]);
-	}
-	
-	for(i=0;i<10;i++)
-	{
-		printf("char is =%d\n",q[i]);
-	}
 	i=0;
 	while(p[i]!=0){
 		i++;
-		printf("i=%d\n",i);
-	}
-	
+	}	
 	j=0;
 	while(q[j] != 0){
 		
 	
 		p[i]=q[j];
-		printf("j=%d",j);
 		j++;
-			i++;
+		i++;
 	}
 	
 }
