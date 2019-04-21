5de927541099bfabd92345d4e9e0b28270dad6b2
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-21 23:05:33 +0530

    String exercise prob1d

---
+++ C/Strings/StringExercise/prob1d.c
@@ -0,0 +1,15 @@
+/* String exercise prob1d */
+#include<stdio.h>
+int main()
+{
+    char s[] = "Churchgate : no church no gate";
+    char t[25];
+    char *ss,*tt;
+    ss = s;
+    while(*ss != '\0')
+    {
+        *tt++ = *ss++;
+    }
+    printf("%s\n",t);
+    return 0;
+}
