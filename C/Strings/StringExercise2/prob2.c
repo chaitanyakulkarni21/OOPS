b82118a2233db7c9126fcbc462f9761d6e0a83af
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-21 23:37:39 +0530

    String Exercise 2 prob2: No error

---
+++ C/Strings/StringExercise2/prob2.c
@@ -0,0 +1,13 @@
+/* String Exercise 2 prob2 */
+#include<stdio.h>
+int main()
+{
+    int arr[] = {'A','B','C','D'};
+    int i;
+    for(i=0;i<=3;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
