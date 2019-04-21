f09bcdf467be4688dc55ca600e767f9b795d5f9c
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-21 23:42:09 +0530

    String exercise 2 : lvalue required error commented

---
+++ C/Strings/StringExercise2/prob3.c
@@ -0,0 +1,13 @@
+/* String Exercise 2 prob3 */
+#include<stdio.h>
+int main()
+{
+    char arr[8] = "Rhombus";
+    int i;
+    for(i=0 ; i<=7 ; i++)
+    {
+        printf("%d",*arr);
+    }
+    arr++;  // lvalue required error
+    return 0;
+}
