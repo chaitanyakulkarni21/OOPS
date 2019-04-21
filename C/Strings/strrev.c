4c017443bcf7cfd541a76d4eb42fb548dfcda4be
chaitanyakulkarni21 <46351531+chaitanyakulkarni21@users.noreply.github.com>
2019-04-21 11:07:37 +0530

    String reverse working

--- C/Strings/strrev.c
+++ C/Strings/strrev.c
@@ -1,21 +1,26 @@
 /* String Reverse */
 #include<stdio.h>
-void strrev(char *);
+#include<string.h>
+void strngrev(char *);
 int main()
 {
 	char str[20];
 	printf("Enter a string: \n");
 	scanf("%s",str);
-	strrev(str);
+	strngrev(str);
 	printf("After Reverse : %s\n",str);
 	return 0;
 }
-void strrev(char* str)
+void strngrev(char* str)
 {
-	int i,count;
-	char st;
-	for(i=0;str[i]!='\0';i++)
+	int i,count,len;
+	char st[20];
+	len=strlen(str);
+	strcpy(st,str);
+	
+	for(i=0,count=len-1;str[i]!='\0';i++,count--)
 	{
-		str[i]=str[i];
+		str[i] = st[count];
 	}
+	//str[i] ='\0';
 }
