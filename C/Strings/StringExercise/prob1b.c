544359b37f8e16971eaf917c689515368fa1001e
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-21 23:00:59 +0530

    String exercise prob1c

---
+++ C/Strings/StringExercise/prob1b.c
@@ -0,0 +1,10 @@
+/* String Exercise prob1b */
+#include<stdio.h>
+int main()
+{
+    char s[] = "Get Organised! Learn C!!";
+    printf("%s\n",&s[2]);
+    printf("%s\n",s);
+    printf("%s\n",&s);
+    printf("%c\n",s[2]);
+}
