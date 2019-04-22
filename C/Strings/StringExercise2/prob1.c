1b769dfc6d2312ea5ed3efc8b0e10b4a79e9df75
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-22 08:15:48 +0530

    String Concatenation using for loop

--- C/Strings/StringExercise2/prob1.c
+++ C/Strings/StringExercise2/prob1.c
@@ -3,9 +3,9 @@
 #include<string.h>
 int main()
 {
-    char *str1[] = "United";
-    char *str2[] = "Front";
-    char *str3[];
+    char *str1 = "United";
+    char *str2 = "Front";
+    char *str3;
     str3 = strcat(str1,str2);
     printf("%s\n",str3);
     return 0;    
--- C/Strings/strcat.c
+++ C/Strings/strcat.c
@@ -3,29 +3,36 @@
 void strcat(char *,char*);
 int main()
 {
-	char str1[20],str2[20],i,l=0;
+	char str1[50],str2[20],i,l=0;
 	printf("Enter string 1 :\n");
 	scanf("%s",str1);
 	printf("Enter string 2 :\n");
 	scanf("%s",str2);
 	strcat(str1,str2);
-	printf("After concatenation: %s",str1);
+	printf("After concatenation: %s\n",str1);
 	return 0;
 }
 void strcat(char *p,char *q)
 {
 	int i,j;
-	i=0;
-	while(p[i]!=0){
-		i++;
-	}	
-	j=0;
-	while(q[j] != 0){
+	// i=0;
+	// while(p[i]!=0){
+	// 	i++;
+	// }	
+	// j=0;
+	// while(q[j] != 0){
 		
 	
+	// 	p[i]=q[j];
+	// 	j++;
+	// 	i++;
+	// }
+	
+	for(i=0;p[i]!='\0';i++);
+	for(j=0;q[j]!='\0';j++)
+	{
 		p[i]=q[j];
-		j++;
 		i++;
 	}
-	
+	p[i] = '\0';
 }
