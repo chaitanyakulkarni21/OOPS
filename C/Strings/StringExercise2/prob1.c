23d0e4fa60de2efb76b4bd750cabe0309a8724f1
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-22 08:36:30 +0530

    String Exercise 2 prob1

--- C/Strings/StringExercise2/prob1.c
+++ C/Strings/StringExercise2/prob1.c
@@ -3,10 +3,11 @@
 #include<string.h>
 int main()
 {
-    char *str1 = "United";
-    char *str2 = "Front";
-    char *str3;
-    str3 = strcat(str1,str2);
-    printf("%s\n",str3);
+    char str1[] = "United";
+    char str2[] = "Front";
+   // char *str3;
+    printf("%s\n%s\n",str1,str2);
+     strcat(str1,str2);     
+    printf("%s\n",str1);
     return 0;    
 }
