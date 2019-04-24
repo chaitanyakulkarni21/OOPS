ea3ec5e0f240f8dedc45b5c7cc7c604cb2dc4f55
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-24 08:59:24 +0530

    Passing Address of a Structure Variable

---
+++ C/Structures/StructureArrays/passingAddresss.c
@@ -0,0 +1,24 @@
+/* Passing address of structure variables */
+#include<stdio.h>
+struct book
+{
+    char name[25];
+    char author[25];
+    int callno;
+};
+void display(struct book *);
+
+int main()
+{
+    struct book b1 = {"Let us C","YPK",101};
+    printf("%u\n",b1);
+    printf("%u\n",b1.name);
+    printf("%u\n",&b1);
+    display(&b1);
+    return 0;
+    
+}
+void display(struct book *b)
+{
+    printf("%s %s %d\n",b->name,b->author,b->callno);
+}
