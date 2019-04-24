7f4e344cc1ab94fdf6919dc833d386548278ade1
Chaitanya Kulkarni <chaitanyapk21@gmail.com>
2019-04-24 08:39:45 +0530

    Different ways of assigning structure elements values

---
+++ C/Structures/StructureArrays/StructFeatures.c
@@ -0,0 +1,31 @@
+/* Additional features of structures */
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    struct employee 
+    {
+        char name[10];
+        int age;
+        float salary;
+    };
+    struct employee e1 = {"Sanjay", 30, 5500.50};
+    struct employee e2,e3;
+
+    /* piece-mealing copy */
+
+    strcpy(e2.name, e1.name);
+
+    /* e2.name = e1.name is wrong */
+
+    e2.age = e1.age;
+    e2.salary = e1.salary;
+
+    /* copying elements in one go */
+
+    e3 = e2;
+
+    printf("%s %d %.2f\n",e1.name,e1.age,e1.salary);
+    printf("%s %d %.2f\n",e2.name,e2.age,e2.salary);
+    printf("%s %d %.2f\n",e3.name,e3.age,e3.salary);
+}
